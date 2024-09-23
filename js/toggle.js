6796c5d5009bc143e8bd4d39c5d9a7fa36a416ca
tayef@gain <tayefhossain200@gmail.com>
2024-09-23 20:06:20 +0600

    add the toggle-functions

---
+++ js/toggle.js
@@ -0,0 +1,6 @@
+document.getElementById("donation-btn").addEventListener("click", () => {
+  showSectionById("donation-section");
+});
+document.getElementById("history-btn").addEventListener("click", () => {
+  showSectionById("history-section");
+});
