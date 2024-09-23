21c6ea5040fb246308a01095284437f03126e885
tayef@gain <tayefhossain200@gmail.com>
2024-09-23 18:25:45 +0600

    add the home-page styel and donate-dutton function

--- js/home.js
+++ js/home.js
@@ -1,1 +1,11 @@
-console.log("hi");
+const totalAmount = getTextFieldValueById("total-amount");
+
+function hadelButtonClick(event, inputId, donateId) {
+  const inputValue = getInputValue(inputId);
+  const totalDonate = getTextFieldValueById(donateId);
+  const newTotalDonate = inputValue + totalDonate;
+  const newTotalAmount = totalAmount - inputValue;
+  document.getElementById("total-amount").innerText = newTotalAmount;
+  document.getElementById("noakhali-total-donate").innerText = newTotalDonate;
+  console.log(event, inputId, donateId);
+}
